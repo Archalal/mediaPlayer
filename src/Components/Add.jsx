f6f4594320ef4c3ec079387affdaa9cf0110290a
Archa lal s <archa0002@gmail.com>
2025-02-12 06:08:10 +0530

    in home page cards where added

--- src/Components/Add.jsx
+++ src/Components/Add.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
 
 const Add = () => {
     const [show, setShow] = useState(false);
@@ -13,15 +15,46 @@ const Add = () => {
     <div className='mainContainer' >
         <div>
             <span style={{fontSize:"25px",fontWeight:"bold"}}>Upload New video</span>
-            <button className='p-2 rounded-circle ' style={{marginLeft:"10px"}}><i className="fa-solid fa-plus"></i></button>
+            <button onClick={handleShow} className='p-2 rounded-circle ' style={{marginLeft:"10px"}}><i className="fa-solid fa-plus"></i></button>
     
         </div>
         <div>
-        <Modal show={show} onHide={handleClose}>
+        <Modal show={show} onHide={handleClose} size='md'>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Uploading video details</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+          <div style={{border:"1px solid",padding:"12px",borderRadius:"10px"}}>
+
+
+          <FloatingLabel
+        controlId="floatingInput"
+        label="Video Caption"
+        className="mb-3"
+      >
+         <Form.Control type="text" placeholder="Video Caption" />
+         </FloatingLabel>
+
+         <FloatingLabel
+        controlId="floatingInput"
+        label="Video image url"
+        className="mb-3"
+      >
+         <Form.Control type="url" placeholder="Video image url" />
+         </FloatingLabel>
+
+         
+         <FloatingLabel
+        controlId="floatingInput"
+        label="Video youtube link"
+        className="mb-3"
+      >
+         <Form.Control type="text" placeholder="Video youtube link" />
+         </FloatingLabel>
+
+
+          </div>
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
--- src/Components/Allvideo.jsx
+++ src/Components/Allvideo.jsx
@@ -1,10 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Modal from 'react-bootstrap/Modal';
+
 
 const Allvideo = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+ 
   return (
     <div>
-      allvideo
+      <h4>All video</h4>
+      <div>
+
+      <Card style={{ width: '18rem' }} onClick={handleShow}  >
+      <Card.Img variant="top" src="https://images.filmibeat.com/img/2024/10/newproject-2024-10-31t112706-026-1730355136.jpg" />
+      <Card.Body>
+       <div className='d-flex justify-content-between'>
+
+       <Card.Title>Card Title</Card.Title>
+      
+      <Button className='btn'>
+        <i className="fa-solid fa-trash text-danger"></i></Button>
+       </div>
+      </Card.Body>
+    </Card>
+
+      </div>
+      <div>
+      <Modal show={show} onHide={handleClose} size='xl' centered>
+        <Modal.Header closeButton>
+          <Modal.Title>VIDEO</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className='border border-dark p-3'>
+          <iframe width="440" height="315" src="https://www.youtube.com/embed/AY3Yea6hBbI?si=i_e1-3BSDr5gF_id" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          </div>
+        </Modal.Body>
+        {/* <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer> */}
+      </Modal>
+     
+
+      </div>
+      
+     
     </div>
+    
   )
 }
 
